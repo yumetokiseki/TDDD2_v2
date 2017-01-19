8ae81a4acf81be16e0d951e7fd92a89115d969ca
yumetokiseki <yumetokiseki@LAPTOP-V8EAP409>
2017-01-19 20:57:10 +0800

    情境二完成

--- ShoppingCart/ShoppingCartCalculator.cs
+++ ShoppingCart/ShoppingCartCalculator.cs
@@ -13,7 +13,21 @@ namespace ShoppingCart
             double discount = 1.0;
             if (books.Count == 1)
             {
+                discount = 1.0;
                 result = books[0].Price * books[0].Count * discount;
+            }
+            else if (books.Count == 2)
+            {
+                discount = 0.95;
+
+                double temptotal = 0;
+
+                foreach (var item in books)
+                {
+                    temptotal += item.Price * item.Count;
+                }
+
+                result = temptotal * discount;
             }
             return result;
         }
