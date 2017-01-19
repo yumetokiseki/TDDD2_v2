8ca7a230257e779a49281adcf8abcca9a3c12789
yumetokiseki <yumetokiseki@LAPTOP-V8EAP409>
2017-01-19 20:47:21 +0800

    情境二設定

--- ShoppingCartTest/UnitTest1.cs
+++ ShoppingCartTest/UnitTest1.cs
@@ -23,5 +23,22 @@ namespace ShoppingCartTest
             //assert
             Assert.AreEqual(expected,actual);
         }
+
+        [TestMethod]
+        public void 情境二第一集1本_第二集1本_預期200元()
+        {
+            var books = new List<Book>
+            {
+                new Book { Name="Vol_1" , Price = 100 , Count = 1 },
+                new Book { Name="Vol_2" , Price = 100 , Count = 1 }
+            };
+            var expected = 200;
+
+            var shoppingCartCalculator = new ShoppingCartCalculator();
+            var actual = shoppingCartCalculator.CalculateTotal(books);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
